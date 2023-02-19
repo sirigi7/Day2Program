8f72de1d7e94f5bea2fe0090a6c0ca0b42cf3268
Sandeep reddy <123892971+sirigi7@users.noreply.github.com>
2023-02-20 01:45:36 +0530

    Add files via upload

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("The sum of the first " + n + " natural numbers is " + sum);
+    }
+}
